0ebf36161257fa68f9d54aee45c7e23f19b3f0c7
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-05-03 23:27:26 +0900

    머신러닝 - 붖꽃 품종 분류2
    머신러닝 - 붖꽃 품종 분류2

---
+++ pandas-iris.py
@@ -0,0 +1,16 @@
+import pandas
+from sklearn import svm, metrics
+
+csv = pandas.read_csv("iris.csv")
+data = csv[["SepalLength", "SepalWidth", "PetalLength", "PetalWidth"]]
+label = csv["Name"]
+
+clf = svm.SVC()
+clf.fit(data, label)
+results = clf.predict([
+    [5.1, 3.0, 1.3, 0.2]
+])
+print(results)
+
+# score = metrics.accuracy_score()
+# print("정답률:", score)
